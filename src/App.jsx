a2af398d5b890dff1d2b2c9fbdc4e7ecfc95a385
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-09-04 19:57:27 +0200

    dodaj random string

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import vratiTekst from "./helper";
 import pismoIliGlava from "coinflipperjs"
+import randomString from "random-string"
 
 function App() {
     return (
         <>
             <h1>Naslov</h1>
             <h2>Pismo ili glava: {pismoIliGlava()}</h2>
+            <h2>Random string: {randomString({ length: 20 })}</h2>
             {["proizvod 1", "proizvod 2", "proizvod 3"].map((proizvod) => {
                 return <h2 key="">{proizvod}</h2>;
             })}
