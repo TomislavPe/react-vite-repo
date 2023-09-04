3bf163b0c83174f6111ecf9ac8822b09944f6b36
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-09-04 19:49:41 +0200

    rjesenje zadatka

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import vratiTekst from "./helper";
+import pismoIliGlava from "coinflipperjs"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Naslov</h1>
+            <h2>Pismo ili glava: {pismoIliGlava()}</h2>
+            {["proizvod 1", "proizvod 2", "proizvod 3"].map((proizvod) => {
+                return <h2 key="">{proizvod}</h2>;
+            })}
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Expedita, quasi nobis iure veniam possimus soluta commodi.
+                Doloremque illo expedita sit?
+                {vratiTekst() + "ssdhfkl"}
+            </p>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/helper.jsx
@@ -0,0 +1,5 @@
+export default function vratiTekst(){
+    return(
+        <div>nesto</div>
+    )
+}
